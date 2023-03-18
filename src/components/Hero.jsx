4a5c40345636e2fa4c3d7810c5094dc84ba96ecd
Nilesh <nilesh147k@gmail.com>
2023-03-19 01:25:46 +0530

    update home page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,24 +3,24 @@ import Typewriter from "typewriter-effect";
 
 const Hero = () => {
     return (
-        <div className='hero-back w-full h-96 '>
+        <div className='hero-back w-full  '>
             <div className='text-center text-5xl text-white p-4 mt-4'>
-            <Typewriter
-                onInit={(typewriter) => {
-                    ["Welcome","Create Your Gate Pass","Send Your Complaints", "& Many More", "Campus Connect"].map((char)=>{
-                        if(char !=="Campus Connect"){
-                        typewriter.typeString(char)
-                        .pauseFor(1000)
-                        .deleteAll()
-                        .start()
-                        } else {
-                            typewriter.typeString(char)
-                            .start()
-                            .stop()
-                        }
-                    })
-                }}
-            />
+                <Typewriter
+                    onInit={(typewriter) => {
+                        ["Welcome Students!", "Send Your Complaints", "Create Your Gate Pass", "& Many More", "Campus Connect"].map((char) => {
+                            if (char !== "Campus Connect") {
+                                typewriter.typeString(char)
+                                    .pauseFor(1000)
+                                    .deleteAll()
+                                    .start()
+                            } else {
+                                typewriter.typeString(char)
+                                    .start()
+                                    .stop()
+                            }
+                        })
+                    }}
+                />
             </div>
         </div>
     )
